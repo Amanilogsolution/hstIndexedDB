edb47118b3307f3d330cd5fd808f094969ff640a
amreshilogsolution <amresh.singh@ilogsolution.com>
2023-01-28 11:35:55 +0530

    2811:07

--- js/qrcode.js
+++ js/qrcode.js
@@ -1,7 +1,34 @@
-function QRScan(){
+function ToogleButton(type){
+    document.getElementById('QRSearch').style.display='block'
+    if(type == 'SKU'){
+        document.getElementById('Skusearch').style.display='block'
+        document.getElementById('Kitsearch').style.display='none'
+        document.getElementById('childCube').value = ''
 
+    }else{
+        document.getElementById('Skusearch').style.display='none'
+        document.getElementById('Kitsearch').style.display='block'
+        document.getElementById('childCube').value = ''
+
+    }
+    console.log(type)
+}
+
+function QRScan(type){
+    if(type == 'SKU'){
+        ScanKit()
+    }else{
+        ScanSKU()
+    }
 }
 
 function ScanKit(){
+  let value=  document.getElementById('childCube').value
+  console.log(value)
+    
+}
+function ScanSKU(){
+    let value=  document.getElementById('childCube').value
+    console.log(value)
     
 }
