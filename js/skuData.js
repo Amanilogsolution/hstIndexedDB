d6c1ee53d6071a7f6d4f5e15feb3c1666b9f67e3
Amanilogsolution <aman@ilogsolution.com>
2023-02-03 17:40:34 +0530

    push

--- js/skuData.js
+++ js/skuData.js
@@ -1,9 +1,8 @@
 (function () {
     
     let TotalData = JSON.parse(localStorage["packSkuDatas"]);
+    let value = Number(localStorage.getItem('SKUDATA'))
     console.log(TotalData)
     const data =  TotalData.filter(data => data.PACK_NO == value)
     console.log(data)
-
-
 })();
