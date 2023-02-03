ca529363859d6324a03e5fe3bfd5403ff5c47b48
amreshilogsolution <amresh.singh@ilogsolution.com>
2023-02-03 17:24:32 +0530

    asdfasdf

--- js/Pack.js
+++ js/Pack.js
@@ -1,4 +1,17 @@
 (function () {
+    
     let pdata = JSON.parse(localStorage["packDatas"]);
-     console.log(pdata);
+    var packCubeData =[];
+    pdata.forEach((value,index) => {
+        packCubeData.push(` 
+            <tr onclick="dataDetils(${value.PACK_NO})">
+            <td>${index+1}</td>
+            <td>${value.PACK_NAME}</td>
+            <td>${value.SKU_QTY}</td>
+            <td>${value.PACK_EXPIRY}</td>
+            </tr>
+		`)
+    }) 
+    let str = packCubeData.toString().replaceAll(',', '');
+    document.getElementById('packData').innerHTML = str
 })();
