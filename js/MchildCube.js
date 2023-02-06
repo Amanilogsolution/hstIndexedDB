2324e1620d914602a187fd83a0421c27f4a13498
Amanilogsolution <aman@ilogsolution.com>
2023-02-06 14:07:15 +0530

    06-02-2023

--- js/MchildCube.js
+++ js/MchildCube.js
@@ -1,44 +1,44 @@
 const idb = window.indexedDB;
-var TotalData 
+var TotalData
 
 
 (function () {
-    const input = Number(localStorage.getItem('MCID'))
-    const ldb = idb.open('CRM', 2);
-    ldb.onsuccess = function () {
-        const db = ldb.result;
-        const txn = db.transaction('tbl_rfid', 'readonly');
-        const store = txn.objectStore('tbl_rfid');
-        const index = store.index('MC_NO');
-        let query = index.getAll(input);
-        query.onsuccess = (event) => {
-            if (!event.target.result) {
-                console.log(`this ${value} not match`)
-            } else {
-                console.log(event.target.result)
-                removeDuplicates(event.target.result)
-          
-            }
-        }
+  const input = Number(localStorage.getItem('MCID'))
+  const ldb = idb.open('CRM', 2);
+  ldb.onsuccess = function () {
+    const db = ldb.result;
+    const txn = db.transaction('tbl_rfid', 'readonly');
+    const store = txn.objectStore('tbl_rfid');
+    const index = store.index('MC_NO');
+    let query = index.getAll(input);
+    query.onsuccess = (event) => {
+      if (!event.target.result) {
+        console.log(`this ${value} not match`)
+      } else {
+        console.log(event.target.result)
+        removeDuplicates(event.target.result)
+
+      }
     }
+  }
 })();
 
 function getUniqueListBy(arr, key) {
-    return [...new Map(arr.map(item => [item[key], item])).values()]
+  return [...new Map(arr.map(item => [item[key], item])).values()]
 }
 
 function removeDuplicates(data) {
-    
-    jsonObject = data.map(JSON.stringify);
-    uniqueSet = new Set(jsonObject);
-    uniqueArray = Array.from(uniqueSet).map(JSON.parse);
-    TotalData =  Array.from(uniqueSet).map(JSON.parse);
 
-    const childCube = getUniqueListBy(uniqueArray,'CC_NO')
-    let childCubeArray = [];   
-     
-       
-        childCubeArray.push(`
+  jsonObject = data.map(JSON.stringify);
+  uniqueSet = new Set(jsonObject);
+  uniqueArray = Array.from(uniqueSet).map(JSON.parse);
+  TotalData = Array.from(uniqueSet).map(JSON.parse);
+
+  const childCube = getUniqueListBy(uniqueArray, 'CC_NO')
+  let childCubeArray = [];
+
+
+  childCubeArray.push(`
         <div class="col-md-4 mb-3">
         <div class="card">
           <div class="card-body-mccube">
@@ -106,7 +106,6 @@ function removeDuplicates(data) {
               <div class="col bg-warning m-1" onclick="packData(${childCube[14]['CC_NO']})">
               ${childCube[14]['CC_NO']},${childCube[14]['CC_NAME']}
               </div>
-              
             </div>
             <div class="row">
               <div class="col bg-warning m-1" onclick="packData(${childCube[15]['CC_NO']})">
@@ -118,7 +117,6 @@ function removeDuplicates(data) {
               <div class="col bg-warning m-1" onclick="packData(${childCube[17]['CC_NO']})">
               ${childCube[17]['CC_NO']},${childCube[17]['CC_NAME']}
               </div>
-              
             </div>
             <div class="row">
               <div class="col bg-warning m-1" onclick="packData(${childCube[18]['CC_NO']})">
@@ -151,50 +149,50 @@ function removeDuplicates(data) {
     <div class="card">
       <div class="card-body-mccube">
           <div class="row">
-            <div class="col bg-warning m-1" onclick="packData(${childCube[24]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[24]['CC_NO']})">
             ${childCube[24]['CC_NO']},${childCube[24]['CC_NAME']}
             </div>
-            <div class="col bg-warning m-1" onclick="packData(${childCube[25]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[25]['CC_NO']})">
             ${childCube[25]['CC_NO']},${childCube[25]['CC_NAME']}
             </div>
-            <div class="col bg-warning m-1" onclick="packData(${childCube[26]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[26]['CC_NO']})">
             ${childCube[26]['CC_NO']},${childCube[26]['CC_NAME']}
             </div>
             
           </div>
           <div class="row">
-            <div class="col bg-warning m-1" onclick="packData(${childCube[27]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[27]['CC_NO']})">
             ${childCube[27]['CC_NO']},${childCube[27]['CC_NAME']}
             </div>
-            <div class="col bg-warning m-1" onclick="packData(${childCube[28]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[28]['CC_NO']})">
             ${childCube[28]['CC_NO']},${childCube[28]['CC_NAME']}
             </div>
-            <div class="col bg-warning m-1" onclick="packData(${childCube[29]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[29]['CC_NO']})">
             ${childCube[29]['CC_NO']},${childCube[29]['CC_NAME']}
             </div>
             
           </div>
           <div class="row">
-            <div class="col bg-warning m-1" onclick="packData(${childCube[30]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[30]['CC_NO']})">
             ${childCube[30]['CC_NO']},${childCube[30]['CC_NAME']}
             </div>
-            <div class="col bg-warning m-1" onclick="packData(${childCube[31]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[31]['CC_NO']})">
             ${childCube[31]['CC_NO']},${childCube[31]['CC_NAME']}
             </div>
-            <div class="col bg-warning m-1" onclick="packData(${childCube[32]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[32]['CC_NO']})">
             ${childCube[32]['CC_NO']},${childCube[32]['CC_NAME']}
             </div>
           
           </div> 
           <div class="row">
-            <div class="col bg-warning m-1" onclick="packData(${childCube[33]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[33]['CC_NO']})">
             ${childCube[33]['CC_NO']},${childCube[33]['CC_NAME']}
             </div>
-            <div class="col bg-warning m-1" onclick="packData(${childCube[34]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[34]['CC_NO']})">
             ${childCube[34]['CC_NO']},${childCube[34]['CC_NAME']}
             </div>
             
-            <div class="col bg-warning m-1" onclick="packData(${childCube[35]['CC_NO']})">
+            <div class="col bg-danger m-1" onclick="packData(${childCube[35]['CC_NO']})">
             ${childCube[35]['CC_NO']},${childCube[35]['CC_NAME']}
             </div>
           </div>
@@ -202,24 +200,23 @@ function removeDuplicates(data) {
     </div>
   </div>
         `)
-     
-    
-      document.getElementById('aaa').innerHTML = childCubeArray;
+
+  document.getElementById('aaa').innerHTML = childCubeArray;
 }
 
 
 
-function packData(value){
-    setTimeout(()=>{
+function packData(value) {
+  setTimeout(() => {
 
-       const data =  TotalData.filter(data => data.CC_NO == value)
-       localStorage["packSkuDatas"] = JSON.stringify(data)
-       const Packdatass = getUniqueListBy(data,'PACK_NO')
-       localStorage["packDatas"] = JSON.stringify(Packdatass)
+    const data = TotalData.filter(data => data.CC_NO == value)
+    localStorage["packSkuDatas"] = JSON.stringify(data)
+    const Packdatass = getUniqueListBy(data, 'PACK_NO')
+    localStorage["packDatas"] = JSON.stringify(Packdatass)
 
-       window.location.href="pack.html"
-    
-    },1000)
+    window.location.href = "pack.html"
+
+  }, 1000)
 }
 
 
