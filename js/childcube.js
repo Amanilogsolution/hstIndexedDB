3119695f54197c7ca94927eba016b329b7deb49e
Amanilogsolution <aman@ilogsolution.com>
2023-02-15 15:43:58 +0530

    07-02-2023

--- js/childcube.js
+++ js/childcube.js
@@ -1,9 +1,75 @@
 const idb = window.indexedDB;
 
 var filtervalue;
+function ChildCube(){
+    let data = document.getElementById('childCube').value
 
-function searchChildCube() {
-    let id = Number(document.getElementById('childCube').value)
+    // data = 'C00000000000000000000005C00000000000000000000001E28069950000400E7CC56D42'
+    let chunks = []
+    const childcubeDropdown = []
+
+    for (var i = 0, charsLength = data.length; i < charsLength; i += 24) {
+		chunks.push(data.substring(i, i + 24));
+	}
+	uniqueArr = removeDuplicates(chunks);
+    console.log(uniqueArr)
+   
+    uniqueArr.forEach(x=>{
+
+        const ldb = idb.open('CRM', 2);
+
+    ldb.onsuccess = function () {
+        const db = ldb.result;
+        const txn = db.transaction('tbl_rfid', 'readonly');
+
+        const store = txn.objectStore('tbl_rfid');
+        const index = store.index('CC_NO');
+        let query = index.get(x);
+
+        query.onsuccess = (event) => {
+
+            if (!event.target.result) {
+                console.log(`this ${x} not match`)
+
+            } else {
+                console.log(event.target.result)
+                childcubeDropdown.push(event.target.result) 
+   
+            }
+        };
+      
+
+        // document.getElementById('search2').style.display = 'flex';
+    }
+
+    })
+    setTimeout(()=>{
+        const dropdown = []
+        console.log(childcubeDropdown)
+
+        dropdown.push(
+            `<option hidden value="">Please Select Child Cube</option>`
+        )
+
+        childcubeDropdown.forEach(x => 
+            dropdown.push(
+                `<option value="${x.CC_EPCNO}">${x.CC_NAME}</option>`
+            )
+             )
+
+             console.log(dropdown)
+             document.getElementById('childcubesearch').style.display='none'
+             document.getElementById('childcubeDropdown').style.display='flex'
+             document.getElementById('selectedChildCube').innerHTML=dropdown
+    },1000)
+    
+
+}
+
+function searchChildCube(e) {
+    console.log(e.target.value)
+    let id = e.target.value
+    console.log(id)
 
 
     const ldb = idb.open('CRM', 2);
@@ -47,17 +113,11 @@ function searchdata() {
         chunks.push(id.substring(i, i + 24));
     }
     uniqueArr = removeDuplicates(chunks);
-    // console.log(uniqueArr)
-
     const data = JSON.parse(localStorage['datas'])
-    
-
     let inventoryMatch = [];
     let inventoryMisMatch = [];
-
     let match = data.filter(e => uniqueArr.includes(e.PACK_EPC))
     let Mismatch = data.filter(e => !uniqueArr.includes(e.PACK_EPC))
-    console.log(match.length)
     let mothercube = match.length>0 ? match[0]['MC_NO'] : '';
     let childcube = match.length>0 ? match[0]['CC_NO'] : '';
     let arr1 = [];
@@ -65,7 +125,6 @@ function searchdata() {
 
 setTimeout(()=>{
     let OtherInventory = uniqueArr.filter((e => !arr1.includes(e)))
-    // console.log('dkjhkdnhvkdh',OtherInventory)
 },1000)
     var result = match.reduce((x,y)=>{
         (x[y.PACK_NAME] = x[y.PACK_NAME] || []).push(y)
@@ -93,12 +152,24 @@ setTimeout(()=>{
      },{}) 
      let mismatchChildCube = Object.keys(resultdata)  
      for(i=0;i<mismatchChildCube.length;i++){
+
+        var resultdatapack = resultdata[`${mismatchChildCube[i]}`].reduce((x,y)=>{
+            (x[y.PACK_EPC] = x[y.PACK_EPC] || []).push(y)
+            return x;
+         },{})
+
+         let result3 = Object.keys(resultdatapack)  
+         console.log(result3)
+         console.log(resultdatapack)
+
+
+         console.log(resultdatapack[`${result3[0]}`][0]['PACK_EPC'])
         console.log(mismatchChildCube[i])
         console.log(resultdata[`${mismatchChildCube[i]}`].length)
         inventoryMisMatch.push(`
-        <tr class="text-dark" style="font-size:14px">
-        <td>${mismatchChildCube[i]}</td>
-        <td>${resultdata[`${mismatchChildCube[i]}`].length}</td>
+        <tr class="text-dark" style="font-size:14px" onClick="match('${resultdatapack[result3[0]][0]['PACK_EPC']}')">
+        <td data-toggle="modal" data-target="#exampleModal">${mismatchChildCube[i]}</td>
+        <td>${result3.length}</td>
         <td>${resultdata[`${mismatchChildCube[i]}`][0]['BATCH_EXPIRY'] }</td>
         </tr>
 		`)
@@ -151,8 +222,6 @@ function match(value){
             })	
 
             let str = SkuData.toString().replaceAll(',', '');
-
-
             document.getElementById('skudatavalue').innerHTML = str;
 
 
