6c17bc0b3a7ed3e90b350079bf8c00e14da03ac9
Amanilogsolution <aman@ilogsolution.com>
2023-02-17 14:31:04 +0530

    push

--- js/childcube.js
+++ js/childcube.js
@@ -1,85 +1,68 @@
 const idb = window.indexedDB;
 
 var filtervalue;
-function ChildCube(){
+function ChildCube() {
     let data = document.getElementById('childCube').value
-
     let chunks = []
     const childcubeDropdown = []
-
     for (var i = 0, charsLength = data.length; i < charsLength; i += 24) {
-		chunks.push(data.substring(i, i + 24));
-	}
-	uniqueArr = removeDuplicates(chunks);   
-    uniqueArr.forEach(x=>{
-
-        const ldb = idb.open('CRM', 2);
-
-    ldb.onsuccess = function () {
-        const db = ldb.result;
-        const txn = db.transaction('tbl_rfid', 'readonly');
-
-        const store = txn.objectStore('tbl_rfid');
-        const index = store.index('CC_NO');
-        let query = index.get(x);
-
-        query.onsuccess = (event) => {
-
-            if (!event.target.result) {
-                console.log(`this ${x} not match`)
-
-            } else {
-                childcubeDropdown.push(event.target.result) 
-   
-            }
-        };
+        chunks.push(data.substring(i, i + 24));
     }
+    uniqueArr = removeDuplicates(chunks);
+    uniqueArr.forEach(x => {
+        const ldb = idb.open('CRM', 2);
+        ldb.onsuccess = function () {
+            const db = ldb.result;
+            const txn = db.transaction('tbl_rfid', 'readonly');
+            const store = txn.objectStore('tbl_rfid');
+            const index = store.index('CC_NO');
+            let query = index.get(x);
 
+            query.onsuccess = (event) => {
+                if (!event.target.result) {
+                    console.log(`this ${x} not match`)
+
+                } else {
+                    childcubeDropdown.push(event.target.result)
+
+                }
+            };
+        }
     })
-    setTimeout(()=>{
+    setTimeout(() => {
         const dropdown = []
 
         dropdown.push(
             `<option hidden value="">Please Select Child Cube</option>`
         )
-
-        childcubeDropdown.forEach(x => 
+        childcubeDropdown.forEach(x =>
             dropdown.push(
                 `<option value="${x.CC_EPCNO}">${x.CC_NAME}</option>`
             )
-             )
-
-             document.getElementById('childcubesearch').style.display='none'
-             document.getElementById('childcubeDropdown').style.display='flex'
-             document.getElementById('selectedChildCube').innerHTML=dropdown
-    },1000)
-    
-
+        )
+        document.getElementById('childcubesearch').style.display = 'none'
+        document.getElementById('childcubeDropdown').style.display = 'flex'
+        document.getElementById('selectedChildCube').innerHTML = dropdown
+    }, 1000)
 }
 
 function searchChildCube(e) {
-    console.log(e.target.value)
     let id = e.target.value
-    console.log(id)
-
-
     const ldb = idb.open('CRM', 2);
 
     ldb.onsuccess = function () {
         const db = ldb.result;
         const txn = db.transaction('tbl_rfid', 'readonly');
-
         const store = txn.objectStore('tbl_rfid');
         const index = store.index('CC_NO');
         let query = index.getAll(id);
-
         query.onsuccess = (event) => {
 
             if (!event.target.result) {
                 console.log(`this ${id} not match`)
 
             } else {
-                
+
                 localStorage['datas'] = JSON.stringify(event.target.result)
             }
         };
@@ -98,8 +81,6 @@ function searchdata() {
     const id = document.getElementById('child').value;
     let uniqueArr = []
     let chunks = []
-
-
     for (var i = 0, charsLength = id.length; i < charsLength; i += 24) {
         chunks.push(id.substring(i, i + 24));
     }
@@ -109,78 +90,67 @@ function searchdata() {
     let inventoryMisMatch = [];
     let match = data.filter(e => uniqueArr.includes(e.PACK_EPC))
     let Mismatch = data.filter(e => !uniqueArr.includes(e.PACK_EPC))
-    let mothercube = match.length>0 ? match[0]['MC_NO'] : '';
-    let childcube = match.length>0 ? match[0]['CC_NO'] : '';
+    let mothercube = match.length > 0 ? match[0]['MC_NO'] : '';
+    let childcube = match.length > 0 ? match[0]['CC_NO'] : '';
     let arr1 = [];
-    for(i=0;i<data.length;i++){ arr1.push(data[i].PACK_EPC)}
+    for (i = 0; i < data.length; i++) { arr1.push(data[i].PACK_EPC) }
 
-setTimeout(()=>{
-    let OtherInventory = uniqueArr.filter((e => !arr1.includes(e)))
-    const datass = []
-
-    OtherInventory.forEach(x=>{
-
-        const ldb = idb.open('CRM', 2);
-
-    ldb.onsuccess = function () {
-        const db = ldb.result;
-        const txn = db.transaction('tbl_rfid', 'readonly');
-
-        const store = txn.objectStore('tbl_rfid');
-        const index = store.index('PACK_EPC');
-        let query = index.get(x);
-
-        query.onsuccess = (event) => {
-
-            if (!event.target.result) {
-                console.log(`this ${x} not match`)
-
-            } else {
-                datass.push(event.target.result) 
-   
+    setTimeout(() => {
+        let OtherInventory = uniqueArr.filter((e => !arr1.includes(e)))
+        const datass = []
+        OtherInventory.forEach(x => {
+            const ldb = idb.open('CRM', 2);
+            ldb.onsuccess = function () {
+                const db = ldb.result;
+                const txn = db.transaction('tbl_rfid', 'readonly');
+                const store = txn.objectStore('tbl_rfid');
+                const index = store.index('PACK_EPC');
+                let query = index.get(x);
+                query.onsuccess = (event) => {
+                    if (!event.target.result) {
+                        console.log(`this ${x} not match`)
+                    } else {
+                        datass.push(event.target.result)
+                    }
+                };
             }
-        };
-    }
+        })
+        setTimeout(() => {
 
-    })
-    setTimeout(()=>{
+            const invdata = []
 
-        console.log(datass)
-        const invdata = []
-
-        for(i=0;i<datass.length;i++){
-            console.log(datass[i].CC_NAME)
-     
-            invdata.push(`
+            for (i = 0; i < datass.length; i++) {
+                invdata.push(`
             <tr class="text-dark" style="font-size:14px" >
             <td data-toggle="modal" data-target="#exampleModal">${datass[i].CC_NAME}</td>
             <td>${datass[i].PACK_NAME}</td>
             <td>${datass[i].PACK_EXPIRY}</td>
-
             </tr> 
             `)
-            document.getElementById('wrongkitsummary').style.display = 'flex'
+                document.getElementById('wrongkitsummary').style.display = 'flex'
+                document.getElementById('wrongKitFound').innerHTML = invdata
+            }
 
-            document.getElementById('wrongKitFound').innerHTML = invdata
-         }
+        }, 1000)
 
-       
-
-    },1000)
-
-},1000)
-    var result = match.reduce((x,y)=>{
+    }, 1000)
+    var result = match.reduce((x, y) => {
         (x[y.PACK_NAME] = x[y.PACK_NAME] || []).push(y)
         return x;
-     },{})
+    }, {})
 
-     let result2 = Object.keys(result)  
-     for(i=0;i<result2.length;i++){
-        var resultdata = result[`${result2[i]}`].reduce((x,y)=>{
+    let result2 = Object.keys(result)
+    let mismatchdatalength = []
+    let matchdatalength = []
+    let matchsum = 0;
+
+    for (i = 0; i < result2.length; i++) {
+        var resultdata = result[`${result2[i]}`].reduce((x, y) => {
             (x[y.PACK_EPC] = x[y.PACK_EPC] || []).push(y)
             return x;
-         },{})
-         let result3 = Object.keys(resultdata)  
+        }, {})
+        let result3 = Object.keys(resultdata)
+        matchdatalength.push(result3.length)
         inventoryMatch.push(`
         <tr class="text-dark" style="font-size:14px" onClick="match('${result[result2[i]][0]['PACK_EPC']}')">
         <td data-toggle="modal" data-target="#exampleModal">${result2[i]}</td>
@@ -188,36 +158,43 @@ setTimeout(()=>{
          <td>${result[`${result2[i]}`][0]['BATCH_EXPIRY']}</td>
         </tr> 
 		`)
-     }
-     var resultdata = Mismatch.reduce((x,y)=>{
+        const matchsum = matchdatalength.reduce((sum, num) => sum + num);
+        document.getElementById('matchdata').innerHTML = matchsum
+    }
+
+
+
+    var resultdata = Mismatch.reduce((x, y) => {
         (x[y.PACK_NAME] = x[y.PACK_NAME] || []).push(y)
         return x;
-     },{}) 
-     let mismatchChildCube = Object.keys(resultdata)  
-     for(i=0;i<mismatchChildCube.length;i++){
+    }, {})
+    let mismatchChildCube = Object.keys(resultdata)
+    for (i = 0; i < mismatchChildCube.length; i++) {
 
-        var resultdatapack = resultdata[`${mismatchChildCube[i]}`].reduce((x,y)=>{
+        var resultdatapack = resultdata[`${mismatchChildCube[i]}`].reduce((x, y) => {
             (x[y.PACK_EPC] = x[y.PACK_EPC] || []).push(y)
             return x;
-         },{})
+        }, {})
 
-         let result3 = Object.keys(resultdatapack)  
-         console.log(result3)
-         console.log(resultdatapack)
+        let result3 = Object.keys(resultdatapack)
+        mismatchdatalength.push(result3.length)
 
 
-         console.log(resultdatapack[`${result3[0]}`][0]['PACK_EPC'])
-        console.log(mismatchChildCube[i])
-        console.log(resultdata[`${mismatchChildCube[i]}`].length)
         inventoryMisMatch.push(`
         <tr class="text-dark" style="font-size:14px" onClick="match('${resultdatapack[result3[0]][0]['PACK_EPC']}')">
         <td data-toggle="modal" data-target="#exampleModal">${mismatchChildCube[i]}</td>
         <td>${result3.length}</td>
-        <td>${resultdata[`${mismatchChildCube[i]}`][0]['BATCH_EXPIRY'] }</td>
+        <td>${resultdata[`${mismatchChildCube[i]}`][0]['BATCH_EXPIRY']}</td>
         </tr>
 		`)
 
-     }
+    }
+
+    let mismatchsum = 0;
+
+    mismatchdatalength.map(x => mismatchsum += x);
+
+    console.log('mismatch',mismatchsum)
 
     let Matchstr = inventoryMatch.toString().replaceAll(',', '');
     let MisMatchstr = inventoryMisMatch.toString().replaceAll(',', '');
@@ -225,20 +202,19 @@ setTimeout(()=>{
     document.getElementById('cCube').innerHTML = childcube;
     document.getElementById('invMatch').innerHTML = Matchstr
     document.getElementById('invMisMatch').innerHTML = MisMatchstr
-    document.getElementById('matchdata').innerHTML = match.length
-    document.getElementById('notmatchdnata').innerHTML = Mismatch.length
+    document.getElementById('notmatchdnata').innerHTML = mismatchsum
     document.getElementById('loading').style.display = 'none';
     document.getElementById('summery').style.display = 'flex';
     document.getElementById('matchsummary').style.display = 'flex';
-    document.getElementById('mismatchsummary').style.display = 'flex';    
+    document.getElementById('mismatchsummary').style.display = 'flex';
 }
-function match(value){
-    var packData =[];
-	 
+function match(value) {
+    var packData = [];
+
     const ldb = idb.open('CRM', 2);
     ldb.onsuccess = function () {
         const db = ldb.result;
-        const txn = db.transaction('tbl_rfid', 'readonly'); 
+        const txn = db.transaction('tbl_rfid', 'readonly');
         const store = txn.objectStore('tbl_rfid');
         const index = store.index('PACK_EPC');
         let query = index.getAll(value);
@@ -250,29 +226,22 @@ function match(value){
             }
         }
     };
-    setTimeout(()=>{
+    setTimeout(() => {
         let SkuData = [];
 
 
-        packData[0].forEach((value , index) => {
+        packData[0].forEach((value, index) => {
             SkuData.push(`  
               <tr>
               <th scope="row"><small>${++index}</small></th>
               <td><small >${value['SKU_NAME']}</small></td>
               <td><small>${value['SKU_QTY']}</small></td> 
             </tr>			 
-            `)			 
-            })	
+            `)
+        })
 
-            let str = SkuData.toString().replaceAll(',', '');
-            document.getElementById('skudatavalue').innerHTML = str;
-
-
-
-    },1000)
-
-
-
-
+        let str = SkuData.toString().replaceAll(',', '');
+        document.getElementById('skudatavalue').innerHTML = str;
+    }, 1000)
 }
 
